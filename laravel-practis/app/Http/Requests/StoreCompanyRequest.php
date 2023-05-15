e2f0fdb315350d1a8a3596b5dd92039b42400f95
shota-watanabe <t8mmmxc11y@yahoo.co.jp>
2023-05-15 17:36:15 +0900

    mod-controllers

--- laravel-practis/app/Http/Requests/StoreCompanyRequest.php
+++ laravel-practis/app/Http/Requests/StoreCompanyRequest.php
@@ -11,7 +11,7 @@ class StoreCompanyRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true; // true に変更
     }
 
     /**
@@ -22,7 +22,7 @@ class StoreCompanyRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'string', 'max:255'],
         ];
     }
 }
