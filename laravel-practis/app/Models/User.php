2f45d2aa41dde9f0b121e29bd0eaffa2c682e66f
shota-watanabe <t8mmmxc11y@yahoo.co.jp>
2023-05-15 15:20:18 +0900

    add-models

--- laravel-practis/app/Models/User.php
+++ laravel-practis/app/Models/User.php
@@ -41,4 +41,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
 }
