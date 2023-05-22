d597988469074f19985045275f2a9787035d2273
shota-watanabe <t8mmmxc11y@yahoo.co.jp>
2023-05-22 15:57:16 +0900

    部署モデル・テーブル実装 リレーション

---
+++ laravel-practis/app/Models/Section.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Section extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public function company(): BelongsTo
+    {
+       return $this->belongsTo(Company::class);
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(Section::class);
+    }
+}
--- laravel-practis/app/Models/User.php
+++ laravel-practis/app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -49,5 +50,10 @@ class User extends Authenticatable
     public function company(): BelongsTo
     {
         return $this->belongsTo(Company::class);
+    }
+
+    public function sections(): BelongsToMany
+    {
+        return $this->belongsToMany(Section::class);
     }
 }
