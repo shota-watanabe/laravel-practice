636b53670750819d3788feb67564a19d9f3bacbc
shota-watanabe <t8mmmxc11y@yahoo.co.jp>
2023-05-22 22:43:07 +0900

    会社一覧画面に部署を表示

--- laravel-practis/resources/views/companies/index.blade.php
+++ laravel-practis/resources/views/companies/index.blade.php
@@ -17,33 +17,71 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            @if(count($companies) >= 1)
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <table>
-                        <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Name</th>
-                            <th>Created At</th>
-                            <th>Updated At</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($companies as $company)
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                @if(count($companies) >= 1)
+                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <table class="min-w-full divide-y divide-gray-200">
+                            <thead class="bg-gray-50">
                             <tr>
-                                <td>{{ $company->id }}</td>
-                                <td>{{ Html::linkRoute('companies.show', $company->name, compact('company')) }}</td>
-                                <td>{{ $company->created_at }}</td>
-                                <td>{{ $company->updated_at }}</td>
-                                <td><button type="button" onclick="location.href='{{ route('companies.sections.create', ['company' => $company->id]) }}'">部署登録</button></td>
+                                <th scope="col"
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    ID
+                                </th>
+                                <th scope="col"
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Name
+                                </th>
+                                <th scope="col"
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Sections
+                                </th>
+                                <th scope="col"
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Created At
+                                </th>
+                                <th scope="col"
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Updated At
+                                </th>
+                                <th scope="col"
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"></th>
                             </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            @else
-                会社は登録されていません。
-            @endif
+                            </thead>
+                            <tbody class="bg-white divide-y divide-gray-200">
+                            @foreach($companies as $company)
+                                <tr>
+                                    <td class="px-6 py-4 whitespace-nowrap">{{ $company->id }}</td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-indigo-600">
+                                        {{ Html::linkRoute('companies.show', $company->name, compact('company')) }}
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        @if(count($company->sections) >= 1)
+                                            <ul class="list-disc list-inside text-indigo-600">
+                                                @foreach($company->sections as $section)
+                                                    <li>{{ Html::linkRoute('companies.sections.edit', $section->name, compact('company', 'section')) }}</li>
+                                                @endforeach
+                                            </ul>
+                                        @else
+                                            -
+                                        @endif
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap">{{ $company->created_at }}</td>
+                                    <td class="px-6 py-4 whitespace-nowrap">{{ $company->updated_at }}</td>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        <a href="{{ route('companies.sections.create', ['company' => $company->id]) }}"
+                                           class="text-indigo-600">部署登録</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                @else
+                    <div class="bg-white px-4 py-5 border-b border-gray-200 sm:px-6">
+                        <p class="text-lg text-gray-500">会社は登録されていません。</p>
+                    </div>
+                @endif
+            </div>
         </div>
     </div>
 </x-app-layout>
